543dad3360179df4bfa78637d28c50172a17a689
howelearn <dxwaa82@gmail.com>
2021-06-20 12:50:04 +0700

    add cacje history

---
+++ calc-web/assets/storage.js
@@ -0,0 +1,52 @@
+const CACHE_KEY = "calculation_history";
+
+function checkForStorage() {
+    return typeof(Storage) !== "undefined"
+}
+
+function putHistory(data) {
+    if (checkForStorage()) {
+        let hisotryData = null;
+            if (localStorage.getItem(CACHE_KEY) === null) {
+                hisotryData = [];
+            } else {
+                hisotryData = JSON.parse(localStorage.getItem(CACHE_KEY));
+            }
+
+            hisotryData.unshift(data);
+
+            if (hisotryData.length > 5) {
+                hisotryData.pop();
+            }
+
+            localStorage.setItem(CACHE_KEY, JSON.stringify(hisotryData));
+    }
+}
+
+function showHistory() {
+    if (checkForStorage()) {
+        return JSON.parse(localStorage.getItem(CACHE_KEY)) || [];
+    } else {
+        return [];
+    }
+}
+
+function renderHistory() {
+    const hisotryData = showHistory();
+    let hisotryData = document.querySelector("#historyList");
+
+    historyList.innerHTML - "";
+
+    for (let history of hisotryData) {
+        let row = document.createElement('tr');
+        row.innerHTML = "<td>" + history.firstNumber + "</td>";
+        row.innerHTML += "<td>" + history.operator + "</td>";
+        row.innerHTML += "<td>" + history.secondNumber + "</td>";
+        row.innerHTML += "<td>" + history.result + "</td>";
+
+        historyList.appendChild(row);
+
+    }
+}
+
+renderHistory();
